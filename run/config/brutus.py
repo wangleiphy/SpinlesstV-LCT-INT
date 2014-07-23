e0b98e067b915c932e27df605b045e1b1d1d1f5b
Lei Wang <wangleiphy@gmail.com>
2014-07-23 15:49:29 +0200

    submit L=6 jobs on brutus to test wall time

--- run/config/brutus.py
+++ run/config/brutus.py
@@ -7,35 +7,36 @@ BCmodifier = "APBCX" #"APBCX"
 
 latticename = 'honeycomb lattice'
 ###############################
-nickname = 'IntE'
+nickname = 'walltime'
 
-Llist = [9]
-Wlist = [9] #Llist 
-Vlist = arange(1.3, 1.41, 0.01)
-#Vlist = arange(0.2, 1.6, 0.2)
+Llist = [6]
+Wlist = [6] #Llist 
+#Vlist = [1.34]
+#Vlist = arange(1.3, 1.41, 0.01)
+Vlist = arange(0.2, 2.2, 0.2)
 
 itime_max = 1<<31
 RECALC_PERIOD = 10
 WRAP_REFRESH_PERIOD = 10
 
-STEPS_PER_BLOCK = 1
+STEPS_PER_BLOCK = 2
 NBLOCKS = 1024
-THERMALIZATION = 10**4          # in unit of block 
-SWEEPS = 10**6                  # in unit of the the whole system 
-MEASUREMENT_PERIOD = 13         # in unit of block
+THERMALIZATION = 2*10**4          # in unit of block 
+SWEEPS = 10**6                    # in unit of the the whole system 
+MEASUREMENT_PERIOD = 13           # in unit of block
 ##############################
 
 tmin = 60
 tmax = 300
 ncores = 16 
-wtime = '12:00'
+wtime = '36:00'
 bin = '../bin/main'
 
 resfolder = '/cluster/work/scr6/lewang/spinlessctbssdata/' + nickname  + '/'
 #h, m = [int(i) for i in wtime.split(':')]
 #Tlimit = max(3600*h + 60*m - int(tmax*2) , 0)
 
-prog = 'mpirun '+ bin  + ' -i '+ str(tmin) + ' -a ' + str(tmax) 
+prog = 'mpirun '+ bin  + ' -i '+ str(tmin) + ' -a ' + str(tmax)  + ' -c '
 
 def submitJob(bin,args,jobname,wtime,run=False,ncores=None, wait=[]):
 
@@ -66,4 +67,4 @@ def submitJob(bin,args,jobname,wtime,run=False,ncores=None, wait=[]):
 
         subprocess.check_call(cmd)
         #time.sleep(2)
-        return None 
+        return [jobname]

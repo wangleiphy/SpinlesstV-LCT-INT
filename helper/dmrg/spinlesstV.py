fa7b062cdec1f7eb1d484fe587a31fca6ccd9cc0
Lei Wang <wangleiphy@gmail.com>
2014-07-29 16:40:19 +0200

    plot nncorr compare with L=32 dmrg

--- helper/dmrg/spinlesstV.py
+++ helper/dmrg/spinlesstV.py
@@ -3,7 +3,7 @@ from subprocess import check_call
 from math import sqrt , cos , sin , pi 
 from numpy import arange 
 
-L = 32
+L = 64 
 W = 1
 N = L*W/2 
 
@@ -12,11 +12,11 @@ M = 3200
 
 parms = []
 #for V in arange(0.0, 2.2, 0.05):
-for V in [3.0, 4.0]:
+for V in [1.0, 2.0, 3.0, 4.0]:
     parms.append({
           'LATTICE_LIBRARY'           : 'mylattices.xml',
           'MODEL_LIBRARY'             : 'mymodels.xml',
-          'LATTICE'                   : 'chainL32APBCX', 
+          'LATTICE'                   : 'chainL64APBCX', 
           'MODEL'                     : "spinless fermions",
 	      'CONSERVED_QUANTUMNUMBERS'  : 'N',
   	      'TRANSLATION_SYMMETRY'      : 'false',

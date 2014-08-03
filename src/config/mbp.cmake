47b7b08e40920aee1b1b9970054010e3e7f042f6
Lei Wang <wangleiphy@gmail.com>
2014-08-03 08:56:21 +0200

    use mkl in mbp

--- src/config/mbp.cmake
+++ src/config/mbp.cmake
@@ -1,1 +1,3 @@
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -I/opt/local/include/eigen3/")
+#set(CMAKE_CXX_FLAGS "-DEIGEN_USE_MKL_ALL -L/opt/intel/composer_xe_2013_sp1.1.103/mkl/lib/ -mkl=sequential -I/opt/intel/composer_xe_2013_sp1.1.103/mkl/include -I/opt/local/include/eigen3/ ${CMAKE_CXX_FLAGS}")
+
+set(CMAKE_CXX_FLAGS "-I/opt/local/include/eigen3/ ${CMAKE_CXX_FLAGS}")

5754cffd3e4a270623f5da2908edfffdadf8a85c
Lei Wang <wangleiphy@gmail.com>
2015-01-12 20:26:24 +0100

    spinlesstV diagonalization at fixed N

--- helper/RestaX/spinlesstV_fixN.py
+++ helper/RestaX/spinlesstV_fixN.py
@@ -1,17 +1,30 @@
 from numpy import zeros  , log , pi , exp 
-from bitops import btest, bget, bset, bclear, fermionParity
+from bitops import btest, bget, bset, bclear, fermionParity, popcount
 
-def buildH(Kmat, V):
+def buildH(Kmat, Ntot, V):
     '''
     -t*hopping +V*(n-0.5) (n-0.5)
     '''
     
     Nsites = Kmat.shape[0]
     Nstates = 1 << Nsites
-    H = zeros((Nstates, Nstates), float)
+    
+    index2state = {}
+    state2index = {}
+    index = 0
+    for state in range(Nstates):
+        if popcount(state) == Ntot:
+            index2state[index] = state 
+            state2index[state] = index 
+            index += 1 
+    
+    print index2state 
+    print state2index 
+    
+    H = zeros((len(index2state), len(index2state)), float)
 
     # loop over all basis states
-    for state in range(Nstates):
+    for index, state in index2state.iteritems():
         #print bin(state)
 
         for s in range(Nsites):
@@ -19,7 +32,7 @@ def buildH(Kmat, V):
                 sj = Kmat.indices[j]  # a neighboring site sj 
 
                 #interaction , if to avoid double counting 
-                if sj > s: H[state, state] += V* (bget(state, s) -0.5) *  (bget(state, sj) -0.5)
+                if sj > s: H[index, index] += V* (bget(state, s) -0.5) *  (bget(state, sj) -0.5)
 
                 #hopping from site s to site sj 
                 if btest(state, s):
@@ -29,47 +42,18 @@ def buildH(Kmat, V):
                         parity2 = parity1 ^ fermionParity(state2,sj)
                         state3=bset(state2,sj)
 
-                        H[state, state3] += Kmat.data[j] * (1-2*parity2)
+                        H[index, state2index[state3]] += Kmat.data[j] * (1-2*parity2)
     
-    return H 
+    return H , index2state 
 
-def calc_RestaX(Kmat, V, beta):
+
+def calc_RestaX(Kmat, Ntot, V):
     '''
     compute renyi EE S2 
     '''
     
     Nsites = Kmat.shape[0]
-    Hmat = buildH(Kmat, V)
-
-    w, v = eigh(Hmat)
-        
-    w -= w[0]
-    weights = exp(-beta * w)
-    Z = weights.sum()  
-    weights = weights/Z 
-
-    Nstates = len(w)
-
-    res = 0.0
-    # loop over all basis states
-    for n in range(Nstates):
-        for state in range(Nstates):
-            phase = 0.0
-            for si in range(Nsites):
-                phase += si * bget(state, si)
-        
-            res +=  exp(1J*2.*pi/Nsites*phase) * v[state, n]**2 * weights[n]
-
-    return res 
-
-
-def calc_RestaX(Kmat, V):
-    '''
-    compute renyi EE S2 
-    '''
-    
-    Nsites = Kmat.shape[0]
-    Hmat = buildH(Kmat, V)
+    Hmat, index2state = buildH(Kmat, Ntot, V)
 
     w, v = eigh(Hmat)
 
@@ -78,15 +62,14 @@ def calc_RestaX(Kmat, V):
 
     res = 0.0
     # loop over all basis states
-    for state in range(Nstates):
+    for index, state in index2state.iteritems():
         phase = 0.0
         for si in range(Nsites):
             phase += si * (bget(state, si) -0.5) 
     
-        res +=  exp(1J*2.*pi/Nsites*phase) * v[state, 0]**2
+        res +=  exp(1J*2.*pi/Nsites*phase) * v[index, 0]**2
 
     return res 
-
 
 
 if __name__=='__main__':
@@ -97,6 +80,7 @@ if __name__=='__main__':
 
     Thop = 1.0
     L = 8
+    N = 4 
 
     #Kinetic energy matrix 
     Kmat = zeros((L, L),float)
@@ -116,7 +100,7 @@ if __name__=='__main__':
 
     #for V in arange(0.0, 4.0, 0.2):
     if True:
-        V = 2.0
-        X = calc_RestaX(Kmat, V)
+        V = 2.0 
+        X = calc_RestaX(Kmat, 4, V)
 
         print V, X.real, X.imag 
